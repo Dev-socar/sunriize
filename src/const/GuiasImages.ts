1b90ec379d67a176e10908ca19dfd725166304da
Dev-socar <omar.fsc19@gmail.com>
2025-06-07 20:20:19 -0600

    feat: Se creo la funcionalidad del hover de los personajes

--- src/const/GuiasImages.ts
+++ src/const/GuiasImages.ts
@@ -3,32 +3,32 @@ import { type GuiaImage } from "@/types/GuiaImage";
 export const guiasImages: GuiaImage[] = [
   {
     tag: "Shotaro",
-    imageBg: "/temp/mario_card.jpg",
-    imageHover: "/temp/mario_hover.png",
+    imageBg: "/temp/personaje_card.webp",
+    imageHover: "/temp/personaje_hover.png",
   },
   {
     tag: "Eunseok",
-    imageBg: "/temp/mario_card.jpg",
-    imageHover: "/temp/mario_hover.png",
+    imageBg: "/temp/personaje_card.webp",
+    imageHover: "/temp/personaje_hover.png",
   },
   {
     tag: "Sungchan",
-    imageBg: "/temp/mario_card.jpg",
-    imageHover: "/temp/mario_hover.png",
+    imageBg: "/temp/personaje_card.webp",
+    imageHover: "/temp/personaje_hover.png",
     },
     {
         tag: "Wonbin",
-        imageBg: "/temp/mario_card.jpg",
-        imageHover: "/temp/mario_hover.png",
+        imageBg: "/temp/personaje_card.webp",
+        imageHover: "/temp/personaje_hover.png",
     },
     {
         tag: "Sohee",
-        imageBg: "/temp/mario_card.jpg",
-        imageHover: "/temp/mario_hover.png",
+        imageBg: "/temp/personaje_card.webp",
+        imageHover: "/temp/personaje_hover.png",
     },
     {
         tag: "Anton",
-        imageBg: "/temp/mario_card.jpg",
-        imageHover: "/temp/mario_hover.png",
+        imageBg: "/temp/personaje_card.webp",
+        imageHover: "/temp/personaje_hover.png",
     }
 ];
